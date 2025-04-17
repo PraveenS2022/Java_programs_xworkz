317577066e19be3668e7cd803f9a2f98f4289b3d
PraveenS2022 <praveens.7349@gmail.com>
2025-04-18 02:58:52 +0530

    Sample

---
+++ abstract/MainRunner/Runner.java
@@ -0,0 +1,12 @@
+package com.Xworkz.MainRunner;
+
+import com.Xworkz.External.BookDetails;
+import com.Xworkz.Internal.Book;
+
+public class Runner {
+    public static void main(String[]args) {
+        BookDetails bookDetails = new BookDetails();
+        bookDetails.price();
+
+    }
+}
